18d81d77d4e64d3d8bf539d91fdb216ed4803083
Mohit Kumar Yadav <36649655+meet2mky@users.noreply.github.com>
2022-03-01 15:15:36 +0530

    Time: 111 ms, Memory: 26.3 MB - LeetHub

---
+++ odd-even-jump/odd-even-jump.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int oddEvenJumps(vector<int>& arr) {
+        int n = size(arr);
+        map<int,int> index;
+        vector<bool> odd(n,false), even(n,false);
+        odd[n-1] = even[n-1] = true;
+        index[arr[n-1]] = n-1;
+        int res = 1;
+        for(int i = n - 2; i >= 0; i--){
+            map<int,int>::iterator upper =  index.upper_bound(arr[i]); //>
+            map<int,int>::iterator lower =  index.lower_bound(arr[i]); //>= 
+            if(upper != index.begin()){
+                upper--;
+                even[i] = odd[(*upper).second]; 
+            }
+            if(lower != index.end()){
+                odd[i] = even[(*lower).second];
+            }
+            res += odd[i];
+            index[arr[i]] = i;
+        }
+        return res;
+    }
+};
