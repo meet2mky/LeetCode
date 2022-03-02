56556c12e91b7717ac18006da77b67691cd35f2f
Mohit Kumar Yadav <36649655+meet2mky@users.noreply.github.com>
2022-03-02 23:25:27 +0530

    Time: 292 ms (33.12%), Space: 83 MB (79.54%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& A) {
-      int n = A.size();
-      for(int i = 0; i < n; i++){
-        while(A[i] > 0 && A[i] <= n && A[A[i]-1] != A[i])
-          swap(A[A[i]-1],A[i]);
-      }
-      for(int i = 1; i <= n; i++){
-        if(A[i-1] != i) return i;
-      }
-      return n+1;
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+    if (n == 0)
+        return 1;
+    int id = 0;
+    while (id < n)
+    {
+        if (nums[id] <= 0 || nums[id] > n || nums[id] - 1 == id)
+        {
+            id++;
+            continue;
+        }
+        // sort this cycle
+        {
+            int num = nums[id];
+            nums[id] = 0;
+            int place = id;
+            while (num > 0 && num <= n && num != place + 1)
+            {
+
+                int new_num = nums[num - 1];
+                place = num - 1;
+                nums[num - 1] = num;
+                num = new_num;
+            }
+            id++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] != i + 1)
+        {
+            return i + 1;
+        }
+    }
+    return n + 1;
     }
 };
