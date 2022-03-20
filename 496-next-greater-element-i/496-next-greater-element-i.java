d4a3df8106e3869666fb19b0e975a8c4bb484b9a
Mohit Kumar Yadav <36649655+meet2mky@users.noreply.github.com>
2022-03-20 20:00:11 +0530

    Time: 7 ms (28.92%), Space: 44.1 MB (54.53%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,21 +1,30 @@
 class Solution {
+    public int[] nextGreaterForAll(int [] nums){
+        int[] nextGreater = new int[nums.length];
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            while(!stack.isEmpty() && nums[i] > nums[stack.peek()]){
+                nextGreater[stack.peek()] = nums[i];
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            nextGreater[stack.peek()] = -1;
+            stack.pop();
+        }
+        return nextGreater;
+    }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer,Integer> indexInNums2 = new HashMap<Integer,Integer>();
         for(int i = 0; i < nums2.length; i++){
             indexInNums2.put(nums2[i], i);
         }
         int[] ans = new int[nums1.length];
+        int [] nextGreater = nextGreaterForAll(nums2);
         for(int i = 0; i < nums1.length; i++){
             int index = indexInNums2.get(nums1[i]);
-            Boolean solved = false;
-            for(int j = index + 1; j < nums2.length; j++){
-                if(nums2[j] > nums2[index]){
-                    ans[i] = nums2[j];
-                    solved = true;
-                    break;
-                }
-            }
-            if(!solved) ans[i] = -1;
+            ans[i] = nextGreater[index];
         }
         return ans;
     }
