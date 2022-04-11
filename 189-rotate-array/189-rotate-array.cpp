18c504da1c29be907e38a6ea43f0f5b51f179022
Mohit Kumar Yadav <36649655+meet2mky@users.noreply.github.com>
2022-04-11 20:40:00 +0530

    Time: 65 ms (7.65%), Space: 24.8 MB (92.61%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,8 +3,17 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        int cycles=__gcd(k,n);
+        for(int i=0;i<cycles;i++){
+            int j=(i+k)%n;
+            int prev=nums[i];
+            while(j!=i){
+                int curr=nums[j];
+                nums[j]=prev;
+                prev=curr;
+                j=(j+k)%n;
+            }
+            nums[i]=prev;
+        }
     }
 };
