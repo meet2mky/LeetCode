f4b2e1042b70a7ee32baf8b76b858eae67603070
Mohit Kumar Yadav <36649655+meet2mky@users.noreply.github.com>
2022-04-15 01:48:14 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (18.92%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -1,36 +1,28 @@
-enum DIR{
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
-
-
-int dr[4]={0,0,-1,1};
-int dc[4]={-1,1,0,0};
-
-DIR turnRight[4]={UP,DOWN,RIGHT,LEFT};
-
-
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> res(n,vector<int>(n,0));
-        int counter=1;
-        int r=0;
-        int c=0;
-        auto safe=[&](int r,int c){
-            return r>=0 && c>=0 && r<n && c<n && res[r][c]==0;
+        vector<int> dr = {0,1,0,-1};
+        vector<int> dc = {1,0,-1,0};
+        int r = 0, c = 0;
+        int dir = 0;
+        int val = 1;
+        vector<vector<int> > ans(n,vector<int>(n,0));
+        auto safe = [&](int i , int j){
+            if(i < 0 || i >= n || j < 0 || j >= n || ans[i][j] > 0) return false;
+            return true;
         };
-        DIR cd=RIGHT;
-        while(counter<=n*n){
-            res[r][c]=counter++;
-            if(!safe(r+dr[cd],c+dc[cd])){
-                cd=turnRight[cd];
+        while(val  <= (n * n)){
+            ans[r][c] = val++;
+            r += dr[dir];
+            c += dc[dir];
+            if(!safe(r,c)){
+                r-=dr[dir];
+                c-=dc[dir];
+                dir = (dir + 1) % 4;
+                r += dr[dir];
+                c += dc[dir];
             }
-            r+=dr[cd];
-            c+=dc[cd];
         }
-        return res;
+        return ans;
     }
 };
